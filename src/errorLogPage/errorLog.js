f1bb6c50e310f31bcc4138d54f81c00f52c23b78
mmminJJeong <flzn0100@gmail.com>
2022-02-09 00:24:20 +0900

    팝업완료
    폼 작업 진행

--- src/errorLogPage/errorLog.js
+++ src/errorLogPage/errorLog.js
@@ -1,30 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import "./errorLog.css";
 import ContentTitle from "../content/ContentTitel";
+import ErrorModal from "../component/errormodalpop";
+import NameInput from "../component/nameinput"
 
-export default function ErrorLog() {
-    return( 
-        <>
-        <div className="content-wrap">
-            <div className="content">
-                <div className="content-main">
-                    <ContentTitle title="Error Log"></ContentTitle>
-                    <div className="left-frame">
-                        <div className="form-frame frame1">
-                            <label>
-                                목록
-                            </label>
+function ErrorLog() {
+  const [modalOpen, setModalOpen] = useState(false);
 
-                            <div className="form-groups">
-                                
-                            </div>
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+  
 
-                        </div>
+
+  return (
+    <>
+      <div className="content-wrap">
+        <div className="content">
+          <div className="content-main">
+            <ContentTitle title="Error Log"></ContentTitle>
+            <div className="left-frame">
+              <React.Fragment>
+                <button className="btn btn-color" onClick={openModal}>모달팝업</button>
+                <ErrorModal
+                  open={modalOpen}
+                  close={closeModal}
+                  header="Error Popup"
+                  main={
+                    <div>
+                      <label>이름</label>
+                      <NameInput />
                     </div>
-
-                </div>
+                  }
+                >
+                </ErrorModal>
+              </React.Fragment>
             </div>
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
+
+export default ErrorLog;
