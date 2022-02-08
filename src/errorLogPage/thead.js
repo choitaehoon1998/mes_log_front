56b1a0468456bdf5b0149a88bfc440285164e5dd
choitaehoon1998 <51841238+choitaehoon1998@users.noreply.github.com>
2022-02-08 15:16:07 +0900

    회사코드로 검색

--- src/errorLogPage/thead.js
+++ src/errorLogPage/thead.js
@@ -1,15 +1,15 @@
-import React from "react"
+import React from "react";
 
 export default function Thead() {
-    return(
-        <thead className="h25">
-            <tr >
-                <th className="w-100 textLeft">NO</th>
-                <th className="textLeft">ID</th>
-                <th className="textLeft">name</th>
-                <th className="w-200 textLeft">Send Date</th>
-                <th className="w-40 textLeft">상세</th>
-            </tr>
-        </thead>
-    );
+  return (
+    <thead className="h25">
+      <tr>
+        <th className="w-100 textCenter">회사코드</th>
+        <th className="w-100 textCenter">유저명</th>
+        <th className="w-100 textCenter">발생일</th>
+        <th className="w-100 textCenter">발생시</th>
+        <th className="w-40 textCenter">상세</th>
+      </tr>
+    </thead>
+  );
 }
