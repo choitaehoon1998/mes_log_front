315d9cb3cd4be69b1c0e0436a1d7a799b82491a7
wjsalsgus1 <97424180+wjsalsgus1@users.noreply.github.com>
2022-02-09 15:57:13 +0900

    .

--- src/errorLogPage/errorLog.js
+++ src/errorLogPage/errorLog.js
@@ -10,7 +10,6 @@ export default function ErrorLog() {
   const [page, setPage] = useState(0);
   const [total, setTotal] = useState(0);
   
-  
   useEffect(() => {
      axios.get('http://a50b-175-119-149-98.ngrok.io/log',{
       params: {size: 10, page: (page)}
@@ -53,22 +52,21 @@ export default function ErrorLog() {
         <div className="content">
           <div className="content-main">
             <ContentTitle title="Error Log"></ContentTitle>
-
             <div className="left-frame">
               <div className="form-frame frame1">
-                  
                 <input type="text" name="keyword" id="search" placeholder="Search"></input>
-
                 <div className="form-groups">
-                
+
                   <label>목록</label>
+
                   <table>
-                    <Thead></Thead>
+                    <Thead />
                     <Tbody props={errors} />
                     <button onClick={onPrev} >이전</button>
                     &nbsp;{page+1} / {total+1}&nbsp;
                     <button onClick={onNext} >다음</button>
                   </table>
+
                 </div>
               </div>
             </div>
