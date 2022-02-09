aa7457ababea6ac0518ca7aea30fb1f7ef2f9592
mmminJJeong <flzn0100@gmail.com>
2022-02-09 17:13:01 +0900

    스타일 수정

--- src/errorLogPage/errorLog.js
+++ src/errorLogPage/errorLog.js
@@ -23,41 +23,44 @@ export default function ErrorLog() {
       })
       .catch((err) => console.log(err));
   }, [searchKeyword]);
-  
+
   useEffect(() => {
-     axios.get('http://a50b-175-119-149-98.ngrok.io/log',{
-      params: {size: 10, page: (page)}
+    axios
+      .get("http://a50b-175-119-149-98.ngrok.io/log", {
+        params: { size: 10, page: page },
       })
       .then((response) => {
-        setTotal(Math.ceil(response.data.totalElements/10)-1 );
+        setTotal(Math.ceil(response.data.totalElements / 10) - 1);
         setErrors(response.data.content);
-        console.log("total",total)
+        console.log("total", total);
       })
-      .catch((err) => console.log(err)); 
+      .catch((err) => console.log(err));
   }, []);
 
-  const onPrev=()=>{
-    if(!(page === 0)){
-      setPage(page>0 ? page-1 : page )
-      axios.get('http://a50b-175-119-149-98.ngrok.io/log',{
-       params: {size: 10, page: (page-1)}
-       })
-       .then((response) => {
-         setErrors(response.data.content);
-       })
-    } 
-  }
-  const onNext=()=>{
-     if(!(page === total)){
-      setPage((page<total) ? page+1 : total )
-      axios.get('http://a50b-175-119-149-98.ngrok.io/log',{
-       params: {size: 10, page: (page+1)}
-       })
-       .then((response) => {
-         setErrors(response.data.content);
-       })
-     }
-  }
+  const onPrev = () => {
+    if (!(page === 0)) {
+      setPage(page > 0 ? page - 1 : page);
+      axios
+        .get("http://a50b-175-119-149-98.ngrok.io/log", {
+          params: { size: 10, page: page - 1 },
+        })
+        .then((response) => {
+          setErrors(response.data.content);
+        });
+    }
+  };
+  const onNext = () => {
+    if (!(page === total)) {
+      setPage(page < total ? page + 1 : total);
+      axios
+        .get("http://a50b-175-119-149-98.ngrok.io/log", {
+          params: { size: 10, page: page + 1 },
+        })
+        .then((response) => {
+          setErrors(response.data.content);
+        });
+    }
+  };
 
   return (
     <>
@@ -77,7 +80,6 @@ export default function ErrorLog() {
                 <label>목록</label>
                 <table>
                   <Thead />
-
                   {errors.map((error) => (
                     <Tbody
                       key={error._id}
@@ -88,13 +90,14 @@ export default function ErrorLog() {
                       link={error._id}
                     ></Tbody>
                   ))}
-
-                    <button onClick={onPrev} >이전</button>
-                    &nbsp;{page+1} / {total+1}&nbsp;
-                    <button onClick={onNext} >다음</button>
-
                 </table>
-
+                <button onClick={onPrev} className="btn-2 btn-color-2">
+                  이전
+                </button>
+                &nbsp;{page + 1} / {total + 1}&nbsp;
+                <button onClick={onNext} className="btn-2 btn-color-2">
+                  다음
+                </button>
               </div>
             </div>
           </div>
