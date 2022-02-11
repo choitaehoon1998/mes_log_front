20239f12e066c49de1b0433e1fc32230c75c880a
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-11 18:06:53 +0900

    react quill

---
+++ src/Page/notice.js
@@ -0,0 +1,101 @@
+import React, { useState, useMemo, useRef } from "react";
+import ContentTitle from "../components/ContentTitle";
+import SearchForm from "../components/SearchForm";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import Input from "../components/errors/errorinput";
+
+const Notis = () => {
+  const quillRef = useRef();
+  const [content, setContent] = useState({
+    header: "",
+    body: "",
+  });
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: [
+        //[{ 'font': [] }],
+        [{ header: [1, 2, 3, 4, false] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image"],
+        [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+        ["clean"],
+      ],
+    };
+  });
+
+  const formats = [
+    //'font',
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "align",
+    "color",
+    "background",
+  ];
+
+  const handleChangeHeader = (e) => {
+    let newContent = { ...content };
+    newContent.header = e;
+    setContent(newContent);
+  };
+
+  const handleChangeBody = (e) => {
+    let newContent = { ...content };
+    newContent.body = e;
+    setContent(newContent);
+    console.log(content);
+    debugger;
+  };
+
+  const submitNotice = () => {
+    // axios put 전송
+    const dataBody = { ...content };
+  };
+
+  return (
+    <>
+      <div className="content-wrap">
+        <div className="content-main">
+          <ContentTitle title="Notice"></ContentTitle>
+          <section>
+            <div className="search-group">
+              <SearchForm
+                title={"글 제목"}
+                onchangeFunction={(e) => {
+                  handleChangeHeader(e);
+                }}
+              ></SearchForm>
+              <div style={{ marginLeft: "auto" }}>
+                <button onClick={submitNotice}>전송</button>
+              </div>
+            </div>
+          </section>
+          <ReactQuill
+            value={content.body}
+            modules={modules}
+            formats={formats}
+            onChange={handleChangeBody}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Notis;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import ErrorLog from "./Page/errorLog";
+import Notice from "./Page/notice";
 
 export default function Router() {
-  return useRoutes([{ path: "/", element: <ErrorLog /> }]);
+  return useRoutes([
+    { path: "/", element: <ErrorLog /> },
+    { path:"/notice", element: <Notice/>}
+]);
 }
